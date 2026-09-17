315a382166d5a5801ee5310f6f6165ca1645f0ab
fakele-ops <fakelefavy619@yahoo.com>
2026-09-17 23:33:54 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -79,6 +79,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'config.middleware.NoIndexMiddleware',  # <-- add this
 ]
 
 ROOT_URLCONF = 'config.urls'
